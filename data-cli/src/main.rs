b7ec8178e9e9091f2f500aa593f48e989aff2a38
Mendy <mendy.man@botpress.com>
2026-04-13 20:35:27 -0400

    remove deploy cmd

--- data-cli/src/main.rs
+++ data-cli/src/main.rs
@@ -1,6 +1,5 @@
 mod build_admin1;
 mod build_data;
-mod deploy;
 mod regen_sqlite;
 
 use clap::{Parser, Subcommand};
@@ -36,26 +35,6 @@ enum Commands {
         #[arg(short, long, default_value = "public/data/cities.jsonl.br")]
         output: String,
     },
-
-    /// Deploy bundle to Cloudflare Pages
-    DeployCfPages {
-        /// Path to bundle zip file
-        #[arg(short, long, default_value = "bundle.zip")]
-        bundle: String,
-
-        /// Cloudflare account ID
-        #[arg(short, long)]
-        account_id: String,
-
-        /// Cloudflare API token
-        #[arg(short, long)]
-        token: String,
-
-        /// Project name
-        #[arg(short, long, default_value = "zman")]
-        project: String,
-    },
-
     /// Regenerate the SQLite database from source data files
     RegenSqlite {
         /// Output path for the SQLite database file
@@ -73,14 +52,6 @@ fn main() {
         }
         Commands::BuildData { db, output } => {
             build_data::build_data(&db, &output);
-        }
-        Commands::DeployCfPages {
-            bundle,
-            account_id,
-            token,
-            project,
-        } => {
-            deploy::deploy(&bundle, &account_id, &token, &project);
         }
         Commands::RegenSqlite { output } => {
             regen_sqlite::regenerate_db(&output);
